a1409ff991f518d94a4894cf01670fb5d7651ceb
K SAMUEL VICTOR <jgmclydia929@gmail.com>
2025-08-13 19:42:41 +0530

    Update AboutSection.jsx

--- components/AboutSection.jsx
+++ components/AboutSection.jsx
@@ -3,23 +3,20 @@ export default function AboutSection() {
     <section className="py-20 px-6 bg-white text-black flex flex-col md:flex-row items-center gap-10">
       <div className="w-full md:w-1/2">
         <img
-          src="/images/About.jpg" // Replace with your actual image path
+          src="/About.jpg" // Replace with your actual image path
           alt="Office"
           className="rounded shadow object-cover w-full h-[300px] md:h-[400px]"
         />
       </div>
       <div className="w-full md:w-1/2">
         <h2 className="text-2xl md:text-3xlfont-bold mb-5">
-          Personal Loans in Hyderabad – Nihal Finserv
+          Hefty: Where strength meets smart engieering.
         </h2>
         <p className="text-sm md:text-base mb-6 leading-relaxed">
-          Looking for a quick and affordable personal loan in Hyderabad? Nihal Finserv is here to help. We offer personalized loan solutions for salaried and self-employed individuals with competitive interest rates and a smooth, hassle-free process.
+          At Hefty, we engineer more than just machines — we build solutions that power productivity. Specializing in advanced crushing and screening equipment, we serve the mining and construction industries with technology-driven, cost-effective, and reliable systems. Our designs focus on power, precision, and performance, ensuring that every operation achieves maximum efficiency and output.
         </p>
         <p className="text-sm md:text-base mb-6 leading-relaxed">
-          Whether it’s for medical bills, education, travel, or urgent expenses — we’re committed to making borrowing simple, fast, and transparent.
-        </p>
-        <p className="text-sm md:text-base mb-6 leading-relaxed">
-          Based in Hyderabad, Nihal Finserv is dedicated to supporting your financial needs with trusted service and minimal paperwork.
+          With a commitment to innovation and durability, Hefty products are built to thrive in demanding environments, delivering consistent results that our customers can depend on nationwide. From mobile track-mounted crushers to integrated screening systems, we provide solutions tailored to your project’s scale and requirements.
         </p>
       </div>
     </section>
